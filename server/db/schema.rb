8ec6ebb477c5f30a8fd640eed950ed44bc1bf069
Mark Lohr <markuslohr@gmail.com>
2015-03-22 23:17:10 -0400

    adding users / admin

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317191516) do
+ActiveRecord::Schema.define(version: 20150321220039) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150317191516) do
     t.datetime "updated_at",             null: false
     t.float    "latitude",   limit: 24
     t.float    "longitude",  limit: 24
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.boolean  "admin",      limit: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
