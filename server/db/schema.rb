ea1885dae3a606c0236d672302772dab1a4b95b9
Mark Lohr <markuslohr@gmail.com>
2015-03-23 15:55:09 -0400

    user / admin creation

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20150323184038) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "admin_profile_name",     limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
